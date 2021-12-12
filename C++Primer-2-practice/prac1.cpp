35f2cf352962630225c3b0d9bf4817a64c883a39
pekokk <zh0208@163.com>
2021-12-12 13:19:34 +0800

    2.7 编程练习-1

---
+++ C++Primer-2-practice/prac1.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Peko Kk. on 2021/12/12.
+//
+
+//编写一个C++程序， 显示姓名和地址
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "603, Lixian Road, Ganjingzi District, Dalian, Liaoning Province, China." << endl;
+    cout << "My name is Pekko";
+}
+
+
